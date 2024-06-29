806bf975333d5a3b1a418475a507c005aca30128
gds4 <gdantas536@gmail.com>
2024-06-29 04:11:52 -0300

    Modificação do cluster kafka

--- cliente/src/infra/provider/kafka/index.ts
+++ cliente/src/infra/provider/kafka/index.ts
@@ -1,12 +1,12 @@
 import { Kafka, logLevel } from 'kafkajs';
 
 const kafka = new Kafka({
-  brokers: ['cute-owl-7069-us1-kafka.upstash.io:9092'],
+  brokers: ['accepted-mastiff-11579-us1-kafka.upstash.io:9092'],
   ssl: true,
   sasl: {
       mechanism: 'scram-sha-256',
-      username: 'Y3V0ZS1vd2wtNzA2OST2mLwjwh7TslVnw-gpqhqE_6RJ0TREGyjqA9hg6_1C0dA',
-      password: 'MjM5ZWZlNDctMDE3Mi00OWY3LTk4MjktOGM3ZmFiNzAyNmE4'
+      username: 'YWNjZXB0ZWQtbWFzdGlmZi0xMTU3OSSGvLhzmvSqKh7RgX12v1gybqniJNm8eH4',
+      password: 'MDRkMjVjN2MtOWQ5Yi00OGYzLWFiMDMtYWJhNzM5MzQyNGVi'
   },
   logLevel: logLevel.ERROR,
 });
--- pedidos/src/infra/provider/kafka/kafka.consumer.ts
+++ pedidos/src/infra/provider/kafka/kafka.consumer.ts
@@ -3,7 +3,7 @@ import { kafka } from "."
 export const kafkaConsumer = async (topic: string, groupId: string) => {
     const consumer = kafka.consumer({groupId: groupId})
     await consumer.connect();
-    await consumer.subscribe({topic, fromBeginning: true})
+    await consumer.subscribe({topic})
 
     return consumer
 }
